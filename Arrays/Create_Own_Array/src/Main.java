c0aaa7bfce3a778bc86ec99b12fbcc819049b3fe
anthonyzero <736252868@qq.com>
2019-11-17 22:27:24 +0800

    数组增强示例

--- Arrays/Create_Own_Array/src/Main.java
+++ Arrays/Create_Own_Array/src/Main.java
@@ -1,10 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-        Array array = new Array();
-        array.addFirst(1);
-        array.addLast(2);
-        array.add(1,3);
+        Array array = new Array(20);
+        for (int i = 0; i < 10; i ++) {
+            array.addLast(i);
+        }
+        array.add(0, 100);
+        array.set(10, 10);
+        System.out.println(array.get(10));
+
+        array.remove(0);
+        array.remove(array.getSize() - 1);
+
+        array.removeElement(2);
         System.out.println(array);
     }
 }
