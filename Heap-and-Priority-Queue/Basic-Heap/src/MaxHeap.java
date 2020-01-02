d42cb4d7c0bb53381370ed8c0d51884dd86c0978
anthonyzero <736252868@qq.com>
2020-01-02 23:14:53 +0800

    向最大堆添加元素

--- Heap-and-Priority-Queue/Basic-Heap/src/MaxHeap.java
+++ Heap-and-Priority-Queue/Basic-Heap/src/MaxHeap.java
@@ -37,4 +37,24 @@ public class MaxHeap<E extends Comparable<E>> {
     private int rightChild(int index) {
         return index * 2 + 2;
     }
+
+
+    // 向堆中添加元素
+    public void addElement(E e) {
+        data.addLast(e); //满二叉树 添加元素就安装层序遍历 从左到右依次添加
+        siftUp(data.getSize() - 1);
+    }
+
+
+    // 指定哪个索引的元素 上浮
+    private void siftUp(int index) {
+        while(index > 0 && data.get(parent(index)).compareTo(data.get(index)) < 0) {
+            // 它的父节点比它自己要小 不满足最大堆的条件 所以交换这两个值
+            E temp = data.get(index);
+            data.set(index, data.get(parent(index))); //当前节点 设置值
+            data.set(parent(index), temp); //父节点 设置值
+
+            index = parent(index);
+        }
+    }
 }
