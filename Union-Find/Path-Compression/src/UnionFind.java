e37bfec6747426cb0d39b4e889bbadc35a5ecabf
anthonyzero <736252868@qq.com>
2020-01-26 18:17:27 +0800

    并查集（第四版） 路径压缩

---
+++ Union-Find/Path-Compression/src/UnionFind.java
@@ -0,0 +1,65 @@
+public class UnionFind implements UF {
+
+    private int[] parent; //parent[i]表示第一个元素所指向的父节点
+    private int[] rank;   //我们并不会维护rank的语意, 也就是rank的值在路径压缩的过程中, 有可能不在是树的层数值
+    // 这也是我们的rank不叫height或者depth的原因, 他只是作为比较的一个标准
+
+    public UnionFind(int size) {
+        parent = new int[size];
+        rank = new int[size];
+        for (int i = 0; i < size; i++) {
+            parent[i] = i; //初始化的时候父节点值是本身 没有相交
+            rank[i] = 1;
+        }
+    }
+
+    @Override
+    public int getSize() {
+        return parent.length;
+    }
+
+    // 查看元素p和元素q是否所属一个集合,直接看根节点是否一致
+    // O(h)复杂度, h为树的高度
+    @Override
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    // 合并元素p和元素q所属的集合
+    // O(h)复杂度, h为树的高度
+    @Override
+    public void unionElements(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if( pRoot == qRoot )
+            return;
+
+        // 根据两个元素所在树的rank不同判断合并方向
+        // 将rank低的集合合并到rank高的集合上
+        if(rank[pRoot] < rank[qRoot])
+            parent[pRoot] = qRoot; //不维护rank
+        else if(rank[qRoot] < rank[pRoot])
+            parent[qRoot] = pRoot;
+        else{ // rank[pRoot] == rank[qRoot]
+            parent[pRoot] = qRoot;
+            rank[qRoot] += 1;
+        }
+
+    }
+
+    //查找元素的根节点值
+    private int find(int p) {
+        if(p < 0 || p >= parent.length)
+            throw new IllegalArgumentException("p is out of bound.");
+
+        // 不断去查询自己的父亲节点, 直到到达根节点
+        // 根节点的特点: parent[p] == p
+        while( p != parent[p] ){
+            // 路径压缩
+            parent[p] = parent[parent[p]];
+            p = parent[p];
+        }
+        return p;
+    }
+}
