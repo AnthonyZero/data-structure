334b265fc98c0db284a69b823aecf6eb6b3099e0
anthonyzero <736252868@qq.com>
2020-01-30 21:16:25 +0800

    红黑树

---
+++ Red-Black-Tree/The-Performance-of-Red-Black-Tree/src/Main.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+
+    // 对于完全随机的数据插入 普通的二叉搜索树很好用 并不一定效率低
+    // 对于查询 修改操作较多的使用情况 avl树很好用
+    // 红黑树综合性能更优（综合增删改查所有操作）2log(n)的高度
+    public static void main(String[] args) {
+        // int n = 20000000;
+        int n = 20000000;
+
+        Random random = new Random(n);
+        ArrayList<Integer> testData = new ArrayList<>(n);
+        for(int i = 0 ; i < n ; i ++)
+            testData.add(random.nextInt(Integer.MAX_VALUE));
+
+        // Test BST
+        long startTime = System.nanoTime();
+
+        BST<Integer, Integer> bst = new BST<>();
+        for (Integer x: testData)
+            bst.add(x, null);
+
+        long endTime = System.nanoTime();
+
+        double time = (endTime - startTime) / 1000000000.0;
+        System.out.println("BST: " + time + " s");
+
+
+        // Test AVL
+        startTime = System.nanoTime();
+
+        AVLTree<Integer, Integer> avl = new AVLTree<>();
+        for (Integer x: testData)
+            avl.add(x, null);
+
+        endTime = System.nanoTime();
+
+        time = (endTime - startTime) / 1000000000.0;
+        System.out.println("AVL: " + time + " s");
+
+
+        // Test RBTree
+        startTime = System.nanoTime();
+
+        RBTree<Integer, Integer> rbt = new RBTree<>();
+        for (Integer x: testData)
+            rbt.add(x, null);
+
+        endTime = System.nanoTime();
+
+        time = (endTime - startTime) / 1000000000.0;
+        System.out.println("RBTree: " + time + " s");
+    }
+}
